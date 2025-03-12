5bc88c9178ad1ddcb7dc7ab5b28be67813dbeeaa
wDeloo <wdelo.hack@gmail.com>
2025-03-12 02:40:52 +0100

    centered dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
+import { useEffect, useRef, useState } from "react"
 import Lower from "./components/pieces/lower"
 import Middle from "./components/pieces/middle"
 import Upper from "./components/pieces/upper"
 import GlobalProvider from "./context/global"
 
 export default function App() {
+    const [margin, setMargin] = useState(0)
+
+    const dashboardRef = useRef<HTMLDivElement>(null)
+
+    function updateMargin() {
+        const dashboard = dashboardRef.current
+        if (!dashboard) return
+
+        const windowHeight = window.innerHeight
+        const dashboardHeight = dashboard.getBoundingClientRect().height
+
+        const margin = (windowHeight - dashboardHeight) / 2
+
+        setMargin(Math.max(margin, 0))
+    }
+
+    useEffect(() => {
+        const dashboard = dashboardRef.current
+        if (!dashboard) return
+
+        const observer = new ResizeObserver(updateMargin)
+
+        observer.observe(dashboard)
+
+        window.addEventListener('resize', updateMargin)
+
+        return () => {
+            observer.disconnect()
+            window.removeEventListener('resize', updateMargin)
+        }
+    }, [])
+
     return (
         <GlobalProvider>
             <main
@@ -14,7 +47,7 @@ export default function App() {
                 }}
             >
                 <div className="w-full h-full backdrop-brightness-75 overflow-y-auto">
-                    <div className="max-w-5xl w-full p-4 backdrop-filter gap-4 flex flex-col mx-auto">
+                    <div ref={dashboardRef} style={{ marginTop: margin, marginBottom: margin }} className="max-w-5xl p-4 h-fit backdrop-filter gap-4 flex flex-col mx-auto">
                         <Upper />
                         <Middle />
                         <Lower />
